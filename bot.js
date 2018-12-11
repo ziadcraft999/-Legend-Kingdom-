6e199e134771ea7bfe5bef5f0514c00efbf86529
zYaDx <45242928+zYaDx@users.noreply.github.com>
2018-12-11 23:30:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.client ();
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
-    
-    client.on('message', message => {
-        if (message.content === 'ping') {
-          message.reply('pong');
-        }
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-// THIS MUST BE THIS WAY
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
